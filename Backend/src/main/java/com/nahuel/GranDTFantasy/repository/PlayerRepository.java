ed5826c4498653858694f43d47446d5da4cc4b2b
Nahuel Urtasun <161488972+nahuel-urtasun@users.noreply.github.com>
2025-05-15 12:00:18 -0300

    Update PlayerRepository.java

--- Backend/src/main/java/com/nahuel/GranDTFantasy/repository/PlayerRepository.java
+++ Backend/src/main/java/com/nahuel/GranDTFantasy/repository/PlayerRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.nahuel.GranDTFantasy.player.Player;
 
-
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +13,8 @@ public interface PlayerRepository extends JpaRepository<Player, String> {
     void deleteByName(String playerName);
 
     Optional<Player> findByName(String playerName);
+
+    List<Player> findByTeamName(String teamName); 
+
+    List<Player> findByPosition(String position); 
 }
