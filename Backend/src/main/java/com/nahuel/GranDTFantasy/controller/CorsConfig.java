66cf14de8d1cbef298acecfc202dbdb7b50cf287
Nahuel Urtasun <161488972+nahuel-urtasun@users.noreply.github.com>
2025-05-16 13:21:02 -0300

    Update CorsConfig.java

--- Backend/src/main/java/com/nahuel/GranDTFantasy/controller/CorsConfig.java
+++ Backend/src/main/java/com/nahuel/GranDTFantasy/controller/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "http://192.168.0.111:3000")
+                        .allowedOrigins("http://localhost:3000")
                  
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
